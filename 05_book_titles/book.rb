3f643984b043ed863a2059bed201a83b63063cb0
Carlos Florez <carlosjflorezh@gmail.com>
2017-02-28 19:59:53 -0400

    Added 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
 # write your code here
+    def title
+        @title
+    end
+    def title=(new_title)
+        words = new_title.split.map do |word|
+            if ['the', 'a', 'an', 'and', 'in', 'of'].include? word
+                word
+            else
+                word.capitalize
+            end
+        end
+        words.first.capitalize!
+        @title = words.join(' ')
+    end
 end
