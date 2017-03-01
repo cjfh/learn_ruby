cb3b1cb8529802ee422abcba35a906243c581992
Carlos Florez <carlosjflorezh@gmail.com>
2017-02-28 21:04:13 -0400

    Added 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,15 @@
 class Timer
-  #write your code here
+    #write your code here
+    def seconds
+        @seconds = 0
+    end
+    def time_string
+        @time_string
+    end
+    def seconds=(ss)
+        hh = ((ss / 3600) % 60).to_s.rjust(2, '0')
+        mm = ((ss / 60) % 60).to_s.rjust(2, '0')
+        ss = (ss % 60).to_s.rjust(2, '0')
+        @time_string = "#{hh}:#{mm}:#{ss}"
+    end
 end
